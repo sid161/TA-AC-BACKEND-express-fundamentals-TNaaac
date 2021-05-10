bdc096d1c6e1e695c36c3df9c93a2b5b8effab0c
siddharth sharma <sharma.siddharth1610@gmail.com>
2021-05-10 22:23:11 +0530

    block-BNaabq

--- block-BNaabq/sample/server.js
+++ block-BNaabq/sample/server.js
@@ -1,18 +1,31 @@
 var express = require('express');
 var app = express();
 var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-app.use(cookieParser);
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static(__dirname + "/public"));
+
+app.use(cookieParser());
+
+app.use((req,res,next) => {
+    console.log(req.cookies);
+})
+
+
 
 app.use('/about', (req,res,next) => {
     res.cookie('username','sid');
+    res.end("about Page");
     next();
 })
+
 
 app.use(logger('dev'))
 
 app.get('/about', (req,res) => {
-
+ res.sendFile(__dirname + '/index.html' )
 })
 
 app.listen(3000, () => {

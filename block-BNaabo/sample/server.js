0eb0a7efb810fb70418761f34ba474f449bd006b
siddharth sharma <sharma.siddharth1610@gmail.com>
2021-05-10 19:53:58 +0530

    block-BNaabo

--- block-BNaabo/sample/server.js
+++ block-BNaabo/sample/server.js
@@ -1,21 +1,22 @@
 var express = require('express');
 var app = express();
 
-app.use((req,res,next) => {
-    console.log(req.method,req.url);
-    next();
-})
+
 
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.use(express.static())
+app.use(express.static(__dirname + "/public"));
 
 app.get('/json', (req,res) => {
     res.send('welcome')
 })
 
 app.post('/json', (req,res) => {
+    console.log(req.body);
+})
+
+app.post('/contact',(req,res) => {
     console.log(req.body);
 })
 
